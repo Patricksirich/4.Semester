dd93ef56f4a923bf3787479b8ecdda1d53fbbfef
Patricksirich <Patricksirich@hotmail.com>
2019-02-07 15:37:01 +0100

    sum ting

---
+++ Angular/Patrick/FindASitter2019CS/src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+
+  }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(3)]], // Multiple validators
+        password: ['', Validators.required] // Single validator
+    }
+    )
+  }
+
+  onSubmit() : void {
+    console.log(this.loginForm);
+
+    if(this.loginForm.valid){
+      //Send the data to the server to verify the user login
+    }
+    else{
+      //Show error msg or something else...
+    }
+  }
+
+}
