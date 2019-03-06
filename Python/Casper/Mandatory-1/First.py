93aeb86a69be9b676e8a56dfb17b6db2a238db7c
Casper <callo2324@gmail.com>
2019-03-06 21:03:12 +0100

    Step 3 complete

--- Python/Casper/Mandatory-1/First.py
+++ Python/Casper/Mandatory-1/First.py
@@ -3,23 +3,40 @@ import os
 import subprocess
 from urllib.request import Request, urlopen
 import urllib.response
+import glob
 
 url = ('https://api.github.com/orgs/python-elective-2-spring-2019/repos?per_page=100')
 res = urlopen(url)
 html = res.read().decode('utf-8')
 
-urls = []
+currentUrls = []
 lines = html.split(',')
 
 for line in lines:
     if 'clone_url' in line:
-        urls.append(line[13:-1])
+        currentUrls.append(line[13:-1])
 
-print(urls)   
+i = 0
+"""
+while i < len(currentUrls):
 
-subprocess.run(['git', 'clone', urls])
+    subprocess.run(['git', 'clone', currentUrls[i]])
+    i += 1
+"""   
 
-if __name__ == "__main__":
-    pass
+readmeFiles = []
+for readme in glob.glob('C:/Users/Callo/OneDrive/Skrivebord/GitHub/4.Semester/Python/Casper/Mandatory-1/*/*.md'):
+        
+        readmeContend = open(readme).read()
+        readmeFiles.append(readmeContend)
+
+requiredList = [i.split('##') for i in readmeFiles]
+
+required = 'Required reading'
+
+if required in requiredList:
+        requiredList.append(required)
+        
+#print(requiredList)
 
 
