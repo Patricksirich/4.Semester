9ede5612bbbecec384120ad2dd17b0b254ba2f16
Patricksirich <Patricksirich@hotmail.com>
2019-03-10 15:26:55 +0100

    asd

--- Python/Patrick/Mandatory/Mandatory.py
+++ Python/Patrick/Mandatory/Mandatory.py
@@ -73,7 +73,7 @@ for long_string in readme:
                         continue
 
                 if single_line[1].islower(): #changes lower-case to upper case (only checks the 3rd char of the string) and then adds the rest of the string
-                        single_line = single_line[0] + single_line[1].upper() + single_line[2:]
+                        single_line = "* " + single_line[0] + single_line[1].upper() + single_line[2:]
 
                 single_line.replace('  ', ' ').replace('   ', ' ').replace('    ', ' ').replace('     ', ' ') #Removes consecutive whitespaces (2, 3, 4 or 5)
                 print('line: ' + single_line)
