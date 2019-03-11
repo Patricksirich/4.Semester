ce47419773849bcf9cbcfdcf93e8926573275447
Casper <callo2324@gmail.com>
2019-03-11 23:52:38 +0100

    NU er det sidste push!!

--- Python/Casper/Mandatory-1/First.py
+++ Python/Casper/Mandatory-1/First.py
@@ -5,31 +5,40 @@ from urllib.request import Request, urlopen
 import urllib.response
 import glob
 
+#To start with, we will indicate where we will collect our data from, save it in a variable, and make sure we can read it
 url = ('https://api.github.com/orgs/python-elective-2-spring-2019/repos?per_page=100')
 res = urlopen(url)
 html = res.read().decode('utf-8')
 
-currentUrls = []
+#Here we split the string up by the ',' token - we will need that later.
 lines = html.split(',')
 
+#We want to place all cloned repositorys in the same folder, so if the folder does not exist, we create a new one.
 if not os.path.exists('clones'):
         os.mkdir('clones')
 os.chdir('clones')
 
+
+#Pull method that checks, if the folder exists, and pulls instead of clones
 for folder_name in lines:
         if 'name' in folder_name:
-                folder_name = folder_name[8:-1]
+                folder_name = folder_name[8:-1]         
         if os.path.exists(folder_name):
                 os.chdir(folder_name)
                 subprocess.run(['git', 'pull', 'origin', 'master'])
-                os.chdir('..')
-                        
+                os.chdir('..')  
+                #When a loop is completed, go one dir up and repeat
+
+
+#If a repository does not exist, clone it from git
+currentUrls = []
+
 if not os.path.exists(folder_name):
-        for clone_url in lines: #goes through all the lines
-                if 'clone_url' in clone_url: #Find all lines that contains the string 'clone_url'
-                        clone_url = clone_url[13:-1] #Remove the '"clone_url": ' and the final ", to ensure that only the URL is left
-                        currentUrls.append(clone_url) #Add the URL to the list
-                        subprocess.run(['git', 'clone', clone_url]) #Clone the repo in our newly created directory
+        for clone_url in lines: 
+                if 'clone_url' in clone_url: 
+                        clone_url = clone_url[13:-1]
+                        currentUrls.append(clone_url)
+                        subprocess.run(['git', 'clone', clone_url])
 
 readmeFiles = []
 for readme in glob.glob('C:/Users/Callo/OneDrive/Skrivebord/GitHub/4.Semester/Python/Casper/Mandatory-1/clones/*/readme.md'):
@@ -56,30 +65,30 @@ os.chdir('curriculum')
 output_list = []
 output_list.append("## Required reading:")
 for long_string in readmeFiles:                
-        for single_line in long_string.split("*"): #Splits it into singlelines so we can check for duplicates
-                single_line = '*' + single_line[0:] # Add a '*' to the beginning of the string to make it a bulletpoint
-                single_line = single_line.strip() #Removes all unnecessary whitespaces
+        for single_line in long_string.split("*"):
+                single_line = '*' + single_line[0:]
+                single_line = single_line.strip()
                 
-                if single_line in output_list: #Checks if the line is a duplicate (does not work)
+                if single_line in output_list:
                         print(single_line + '     :Exists!')
                         continue
 
-                if len(single_line) < 5: #Skips empty lines
+                if len(single_line) < 5:
                         continue
 
-                if single_line[3].islower(): #changes lower-case to upper case (only checks the 3rd char of the string) and then adds the rest of the string
+                if single_line[3].islower():
                         single_line = single_line[0:3] + single_line[3].upper() + single_line[4:]
 
-                single_line.replace('  ', ' ').replace('   ', ' ').replace('    ', ' ').replace('     ', ' ') #Removes consecutive whitespaces (2, 3, 4 or 5)
+                single_line.replace('  ', ' ').replace('   ', ' ').replace('    ', ' ').replace('     ', ' ')
                 
                 output_list.append(single_line)
 
-file = open('Required_reading.md', 'w') #Create new file 
-large_string = "\n".join(sorted(output_list)) #Sorts the list
-file.write(large_string) #Writes to the file
-file.close() #Close the file for good practice
+file = open('Required_reading.md', 'w')
+large_string = "\n".join(sorted(output_list))
+file.write(large_string)
+file.close()
 
 os.chdir('..')
-comment = input('Please enter a comment for the push: ')
+comment = input('Comment your push: ')
 subprocess.run(['git', 'commit', '-am', comment])
 subprocess.run(['git', 'push', 'origin', 'master'])
