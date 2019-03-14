1eae6e80f49e10eefac99c9c5d96d12c93187251
MatthiasSkou <ms@klinikskou.dk>
2019-03-14 14:58:02 +0100

    yarp

--- Angular/Matthias/FindASitter2019CS/src/app/display-quiz/display-quiz.component.ts
+++ Angular/Matthias/FindASitter2019CS/src/app/display-quiz/display-quiz.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TempDataService } from '../service/temp-data.service';
 import { Quiz } from '../entities/quiz';
+import { Identifiers } from '@angular/compiler';
+import { Route, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-display-quiz',
@@ -10,11 +12,12 @@ import { Quiz } from '../entities/quiz';
 export class DisplayQuizComponent implements OnInit {
   quiz: Quiz;
 
-  constructor(private tempData: TempDataService) { }
+  constructor(private tempData: TempDataService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = '1';
-    this.quiz = this.tempData.findQuiz(id);
+
+    const id = this.route.snapshot.paramMap.get('id')
+    this.quiz = this.tempData.findQuiz(id); 
     // get ID from URL
     // find quiz object based on ID
     // load quiz in HTML
