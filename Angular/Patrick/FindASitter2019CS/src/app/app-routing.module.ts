33cb7b92d3dce30b477d3cea51d5c10885010286
Patricksirich <Patricksirich@hotmail.com>
2019-03-14 11:03:34 +0100

    aner ikke hvad det her er

--- Angular/Patrick/FindASitter2019CS/src/app/app-routing.module.ts
+++ Angular/Patrick/FindASitter2019CS/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { PortalComponent } from './portal/portal.component';
 import { AuthGuard } from './auth/auth.guard';
 import { AdminGuard} from './admin/admin.guard';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home/index', pathMatch: 'full' }, // if baseUrl => go to login
@@ -25,7 +26,7 @@ const routes: Routes = [
   {path: 'portal', component: PortalComponent, canActivate: [AuthGuard], children: [
       {path: 'display-quiz', component: DisplayQuizComponent },  
   ]},
-  {path: 'admin', component: HomeComponent, canActivate: [AdminGuard], children: [
+  {path: 'admin', component: AdminComponent, canActivate: [AdminGuard], children: [
     {path: 'admin-panel', component: AdminPanelComponent}
   ]},
 
