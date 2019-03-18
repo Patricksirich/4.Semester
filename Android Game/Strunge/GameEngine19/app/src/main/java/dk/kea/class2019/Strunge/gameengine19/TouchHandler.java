9fe9d836aa32c1fe74c060aae241d06570efa485
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2019-03-18 11:29:48 +0100

    Added gameEngine

---
+++ Android Game/Strunge/GameEngine19/app/src/main/java/dk/kea/class2019/Strunge/gameengine19/TouchHandler.java
@@ -0,0 +1,8 @@
+package dk.kea.class2019.Strunge.gameengine19;
+
+public interface TouchHandler
+{
+    public boolean isTouchDown(int pointer);
+    public int getTouchX(int pointer);
+    public int getTouchY(int pointer);
+}
