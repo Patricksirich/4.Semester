8c3e45d0f4869169f838dbf064207961e46d4be0
Casper <callo2324@gmail.com>
2019-03-26 12:31:56 +0100

    l8st push

--- Python/Casper/Python files/Lesson-11-12/circle.py
+++ Python/Casper/Python files/Lesson-11-12/circle.py
@@ -1,11 +1,17 @@
 from math import pi
 
 def area(r):
+    if r < 0:
+        raise ValueError('You can not have a circle with a negative radius')
     return pi * r**2
 
-print(area(2))
 
-list = [1, 0, -3, True, 'Hello']
+def main():
+    list = [1, 0, -3, True, 'Hello']
 
-for value in list:
-    print(f'Area of Circle with r = {value} is {area(value)}')
+    for value in list:
+        print(f'Area of Circle with r = {value} is {area(value)}')
+
+
+if __name__ == "__main__":
+    main()
--- Python/Casper/Python files/Lesson-11-12/test_circle.py
+++ Python/Casper/Python files/Lesson-11-12/test_circle.py
@@ -7,5 +7,9 @@ class TestCircle(unittest.TestCase):
     def test_if_area_is_positive(self):
         
         # Test when area is larger than 0
-        self.assertAlmostEqual()
-    
+        self.assertAlmostEqual(area(1), pi)
+        self.assertAlmostEqual(area(0), 0.0)
+        self.assertAlmostEqual(area(2.2), pi*2.2**2)
+
+    def test_values(self):
+            self.assertRaises(ValueError, area, -2)
