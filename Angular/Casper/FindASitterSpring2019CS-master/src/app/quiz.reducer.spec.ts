1e4ca2276bdb83c5d24f3e0af8dc08329413c78b
Casper <callo2324@gmail.com>
2019-04-25 12:22:35 +0200

    Unit tests on quiz reducer

--- Angular/Casper/FindASitterSpring2019CS-master/src/app/quiz.reducer.spec.ts
+++ Angular/Casper/FindASitterSpring2019CS-master/src/app/quiz.reducer.spec.ts
@@ -1,5 +1,7 @@
 import { quizReducer } from "./quiz.reducer";
 import { QuizActions } from './quiz.actions';
+import { QuizState } from './store';
+import { Quiz } from './entities/quiz';
 var deepFreeze = require('deep-freeze');
 
 fdescribe('quiz-reducer-tests', () => {
@@ -14,4 +16,17 @@ fdescribe('quiz-reducer-tests', () => {
 
       expect(newStateObj).toEqual({ isLoggedIn: true, quizzes: [] });
     });
+
+    it('should create new', () => {
+      let startState = {quizzes: []} as QuizState;
+      deepFreeze(startState);
+
+      let quiz = { title: 'test quiz', questions: []} as Quiz;
+      let actionObj = { type: QuizActions.NEW_QUIZ, payload: quiz};
+      let newStateObj = quizReducer(startState, actionObj);
+
+      expect(newStateObj.quizzes[0].title).toBe('test quiz')
+      expect(newStateObj.quizzes.length).toBe(1);
+    })
 })
+
