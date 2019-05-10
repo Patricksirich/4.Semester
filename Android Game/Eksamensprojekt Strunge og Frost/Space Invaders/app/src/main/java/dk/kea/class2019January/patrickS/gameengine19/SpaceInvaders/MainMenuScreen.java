9ca0abb34b68c999bae223679f356633a47fa6a5
ortismal <33696981+ortismal@users.noreply.github.com>
2019-05-10 13:37:50 +0200

    Main menu uden title

--- Android Game/Eksamensprojekt Strunge og Frost/Space Invaders/app/src/main/java/dk/kea/class2019January/patrickS/gameengine19/SpaceInvaders/MainMenuScreen.java
+++ Android Game/Eksamensprojekt Strunge og Frost/Space Invaders/app/src/main/java/dk/kea/class2019January/patrickS/gameengine19/SpaceInvaders/MainMenuScreen.java
@@ -1,6 +1,8 @@
 package dk.kea.class2019January.patrickS.gameengine19.SpaceInvaders;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Typeface;
 
 import dk.kea.class2019January.patrickS.gameengine19.Breakout.GameScreen;
 import dk.kea.class2019January.patrickS.gameengine19.GameEngine;
@@ -12,17 +14,25 @@ public class MainMenuScreen extends Screen{
     private Bitmap startGame;
     float passedTime = 0;
     long startTime;
+    String titleText;
+    Typeface font;
 
     public MainMenuScreen(GameEngine gameEngine) {
 
         super(gameEngine);
         startGame = gameEngine.loadBitmap("SpaceInvaders/xstartgame.png");
         mainMenu = gameEngine.loadBitmap("SpaceInvaders/background.png");
+        font = gameEngine.loadFont("SpaceInvaders/font.ttf");
         startTime = System.nanoTime();
     }
 
+    // TODO:
+    //  titleText = "Space Invaders";
+    //  gameEngine.drawText(font, titleText, 50, 70, Color.GREEN, 11);
+
     @Override
     public void update(float deltaTime) {
+
 
         if (gameEngine.isTouchDown(0) && (passedTime) > 0.5f) {
             gameEngine.setScreen(new GameScreen(gameEngine));
@@ -33,7 +43,6 @@ public class MainMenuScreen extends Screen{
         if ((passedTime - (int)passedTime) > 0.5f) {
             gameEngine.drawBitmap(startGame, 240 - startGame.getWidth()/2, 160);
         }
-
     }
 
     @Override
@@ -50,6 +59,7 @@ public class MainMenuScreen extends Screen{
     public void dispose() {
 
     }
+
 
 
 }
