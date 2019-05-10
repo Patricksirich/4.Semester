ef1f60944ccb6acf693f6ea8de074ede8d3a9dd6
ortismal <33696981+ortismal@users.noreply.github.com>
2019-05-10 13:19:22 +0200

    MainMenu

--- Android Game/Eksamensprojekt Strunge og Frost/Space Invaders/app/src/main/java/dk/kea/class2019January/patrickS/gameengine19/SpaceInvaders/MainMenuScreen.java
+++ Android Game/Eksamensprojekt Strunge og Frost/Space Invaders/app/src/main/java/dk/kea/class2019January/patrickS/gameengine19/SpaceInvaders/MainMenuScreen.java
@@ -1,4 +1,55 @@
 package dk.kea.class2019January.patrickS.gameengine19.SpaceInvaders;
 
-public class MainMenuScreen {
+import android.graphics.Bitmap;
+
+import dk.kea.class2019January.patrickS.gameengine19.Breakout.GameScreen;
+import dk.kea.class2019January.patrickS.gameengine19.GameEngine;
+import dk.kea.class2019January.patrickS.gameengine19.Screen;
+
+public class MainMenuScreen extends Screen{
+
+    private Bitmap mainMenu;
+    private Bitmap startGame;
+    float passedTime = 0;
+    long startTime;
+
+    public MainMenuScreen(GameEngine gameEngine) {
+
+        super(gameEngine);
+        startGame = gameEngine.loadBitmap("SpaceInvaders/xstartgame.png");
+        mainMenu = gameEngine.loadBitmap("SpaceInvaders/background.png");
+        startTime = System.nanoTime();
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+        if (gameEngine.isTouchDown(0) && (passedTime) > 0.5f) {
+            gameEngine.setScreen(new GameScreen(gameEngine));
+            return;
+        }
+        gameEngine.drawBitmap(mainMenu, 0, 0);
+        passedTime = passedTime + deltaTime;
+        if ((passedTime - (int)passedTime) > 0.5f) {
+            gameEngine.drawBitmap(startGame, 160 - startGame.getWidth()/2, 320);
+        }
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+
 }
