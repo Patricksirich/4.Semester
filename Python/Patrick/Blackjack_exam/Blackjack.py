05a3a2e9ad8dbd3a17b87321e3dbf44f5ecddf2a
Patrick <Patricksirich@hotmail.com>
2019-05-21 19:43:45 +0200

    cleanup

--- Python/Patrick/Blackjack_exam/Blackjack.py
+++ Python/Patrick/Blackjack_exam/Blackjack.py
@@ -4,23 +4,21 @@ card_ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 card_suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
 deck = []
 
-#creates the deck with 52 cards
+#Creates the deck with 52 cards
 def create_deck():
     for i in card_suits:
         for j in card_ranks:
             deck.append(j + ' ' + i)
-    return deck
-
-
-#calls the create_deck method and shuffles the cards into our deck array
-def shuffle_deck():
-    deck = create_deck()
+#Shuffle the cards to create a random order in the deck
     shuffle(deck)
     print(deck)
     return deck
+    
 
-shuffle_deck()
 
+
+
+create_deck()
 
 if __name__ == "__main__":
     pass
