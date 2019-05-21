961a4190fb10381a6d16174a9ecb255a0a54c854
Patrick <Patricksirich@hotmail.com>
2019-05-21 19:56:47 +0200

    continue later

--- Python/Patrick/Blackjack_exam/Blackjack.py
+++ Python/Patrick/Blackjack_exam/Blackjack.py
@@ -1,6 +1,6 @@
 from random import shuffle
 
-card_ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+card_ranks = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13}
 card_suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
 deck = []
 
@@ -8,17 +8,22 @@ deck = []
 def create_deck():
     for i in card_suits:
         for j in card_ranks:
-            deck.append(j + ' ' + i)
+            deck.append(j + ' of ' + i)
 #Shuffle the cards to create a random order in the deck
     shuffle(deck)
     print(deck)
     return deck
+
+#Removes 1 card from the deck and return the value
+def deal():
+    single_card = deck.pop()
+    print(single_card)
+    return single_card
     
 
-
-
-
 create_deck()
+deal()
+
 
 if __name__ == "__main__":
     pass
