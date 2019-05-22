d9c1b4094a33312aff29031ba577a57ee099b4fe
ortismal <33696981+ortismal@users.noreply.github.com>
2019-05-22 10:25:59 +0200

    Fjernet angular baggrund

--- Python/Casper/Python files/exam-projects/blackjack/Deck.py
+++ Python/Casper/Python files/exam-projects/blackjack/Deck.py
@@ -1,16 +1,13 @@
 class Deck:
 
-    # Make two arrays with rank and suit, and shuffle a deck with 52 cards in it (no duplicates)
-    
-    # Initialize class with an empty dictionary and the build method
-    def __init__(self):
-        self.cards = []
+    def __init__(self):                                                 # Initialize class with an empty dictionary
+        self.cards = {}
 
     # Build a new deck of 52 unique cards
     def build(self):
-        for i in ["Clubs", "Diamond", "Spades", "Hearts"]:
-            for j in range(1, 14):
-                print(j)
+        for suit in {"Clubs", "Diamond", "Spades", "Hearts"}:
+            for rank in range(1, 14):                                
+                print("{} of {}".format(rank, suit))
     
 deck = Deck()
 deck.build()
