e5360f0f22843c81f6da5c4050143828cee0b0a7
MatthiasSkou <ms@klinikskou.dk>
2019-05-23 17:42:02 +0200

    android game ez clap

--- Android Game/Matthias/app/src/main/java/dk/kea/class2019January/patrickS/gameengine19/SpaceX/Projectile.java
+++ Android Game/Matthias/app/src/main/java/dk/kea/class2019January/patrickS/gameengine19/SpaceX/Projectile.java
@@ -6,7 +6,6 @@ public class Projectile
     public static float WIDTH = 15;
     public static float HEIGHT = 15;
     public float x = 160;
-    public float y = 240;
-    public float vx = 150;
+    public float y = 300;
     public float vy = -150;
 }
--- Angular/Strunge/FindASitterSpring2019CS-master/src/app/update-quiz/update-quiz.component.ts
+++ Angular/Strunge/FindASitterSpring2019CS-master/src/app/update-quiz/update-quiz.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { NgRedux } from '@angular-redux/store';
 import { AppState } from '../store';
 import { Quiz, Question, Option } from '../entities/quiz';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Gender } from '../entities/user';
 
 @Component({
@@ -14,26 +14,22 @@ import { Gender } from '../entities/user';
 export class UpdateQuizComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private ngRedux: NgRedux<AppState>, private fb: FormBuilder) { }
-
-  public quizzes: Quiz[]
-  public questions: Question[]
-  public options: Option[]
   public quizForm: FormGroup
 
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id')
     this.ngRedux.select(state => state.quizzes).subscribe(result => {
-      this.quizzes = result.quizzes
-      let quiz = this.quizzes.find(quiz => quiz._id === id)
+      let quizzes = result.quizzes
+      let quiz = quizzes.find(quiz => quiz._id === id)
+      console.log(quiz)
+      console.log(quiz.questions)
+
 
       this.quizForm = this.fb.group({
         title: [quiz.title],
-        questions: this.fb.array(this.questions),
-        options: this.fb.array(this.options),
-
-
-      })
+        questions: this.fb.array(quiz.questions)
+      });
     })
   }
   
@@ -48,5 +44,30 @@ export class UpdateQuizComponent implements OnInit {
     gender: Gender.MALE,
     birthDate: undefined
    };
+  }
+
+  createNewQuestion() {
+    const question = this.fb.group({
+      title: ['', Validators.required],
+      options: this.fb.array([])
+    });
+
+    const questions = this.quizForm.controls.questions as FormArray;
+    const options = question.controls.options as FormArray;
+    options.push(this.createNewOptionGroup());
+    options.push(this.createNewOptionGroup());
+    questions.push(question);
+  }
+  createNewOption(questionIndex: number){
+    const option = this.createNewOptionGroup();
+    const questions = this.quizForm.controls.questions as FormArray;
+    const options = (<FormArray>questions.controls[questionIndex]).controls['options'] as FormArray;
+    options.push(option);
+  }
+  private createNewOptionGroup(): FormGroup {
+    return this.fb.group({
+      answer: ['', Validators.required],
+      correct: [false, Validators.required]
+    });
   }
 }
