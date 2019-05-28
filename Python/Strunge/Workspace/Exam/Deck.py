d91b607e909b1f42c059e7c5f055a21262451cea
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2019-05-28 14:06:37 +0200

    Python updated

--- Python/Strunge/Workspace/Exam/Deck.py
+++ Python/Strunge/Workspace/Exam/Deck.py
@@ -6,7 +6,7 @@ class Deck:
 
     def createDeck(self):
         deckValues = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-        deckSuits = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+        deckSuits = ['♥', '♠', '♦', '♣']
 
         for suit in deckSuits:
             for number in deckValues:
