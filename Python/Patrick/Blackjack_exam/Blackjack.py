7c5bba22816185498e8701bcc0845739e2ff69ef
Patrick <Patricksirich@hotmail.com>
2019-05-28 15:43:02 +0200

    Mangler kun at formatere

--- Python/Patrick/Blackjack_exam/Blackjack.py
+++ Python/Patrick/Blackjack_exam/Blackjack.py
@@ -14,10 +14,8 @@ dealer_cards = []
 player_cards = []
 
 #Global variables to check the value of dealer and players cards
-player_value_global = 0
-dealer_value_global = 0
-
-chips = 0
+#player_value_global = 0
+#dealer_value_global = 0
 
 #Creates the deck with 52 cards
 def create_deck():
@@ -27,33 +25,32 @@ def create_deck():
             deck.append(ranks + " of " + suits)
 #Shuffle the cards to create a random order in the deck
     shuffle(deck)
-    print(deck)
     return deck
+
 
 #Removes last card from the deck[] and return the value
 def deal():
     single_card = deck.pop()
     return single_card
 
+
 #Checks the card's value so that we can do a sum of the cards in the hand
 def card_value(card_name):
     card_rank = card_name.split(" of ")
     return card_values[card_rank[0]]
 
+
 #Checks whether the picked card is an Ace and either add 1 or the predefined value
-def check_for_ace(card_name, current_value):
-    print(card_name.split(" of ")[0])
-    
+def check_for_ace(card_name, current_value):    
     if card_name.split(" of ")[0] == "A" and current_value + 11 > 21:
         return_value = 1
     else:
         return_value = card_value(card_name)
-    print("You have drawn a: ", card_name, " With the value: ", return_value)
     return return_value
 
 
 #Rules for dealing the cards as a player
-def player_deal(player_value_local = player_value_global):
+def player_deal(player_value_local):
     playing = True
     current_value = player_value_local
 
@@ -69,7 +66,6 @@ def player_deal(player_value_local = player_value_global):
             for i in player_cards:
                 player_value_local += check_for_ace(i, current_value)
 
-            print("local: ", player_value_local, "global: " , player_value_global)
             print(player_cards)
             current_value = player_value_local
 
@@ -85,13 +81,17 @@ def player_deal(player_value_local = player_value_global):
     #Return the local variable in the function so that we can change the global variable
     return player_value_local
 
+
 #Rules for dealing the cards for the player
-def dealer_deal(dealer_value_local = dealer_value_global):
+def dealer_deal(dealer_value_local):
     current_value = dealer_value_local
     print(dealer_value_local)
     #Forever loop so that we don't have to take any actions
     #Rules of blackjack states that dealer must hit when sum <= 16
     while True:
+        #check if dealer goes above 16, if true then break the loop
+        if dealer_value_local > 16:
+            break 
         dealer_cards.append(deal())
         dealer_value_local = 0
         #Iterate through dealer_cards and find the value so that we can check the sum of the cards - also checks for aces
@@ -100,37 +100,38 @@ def dealer_deal(dealer_value_local = dealer_value_global):
         print(dealer_value_local)
         current_value = dealer_value_local
 
-        #check if dealer goes above 16, if true then break the loop
-        if dealer_value_local > 16:
-            break
-
     #Return the local variable in the function so that we can change the global variable
     return dealer_value_local
 
 
 #Function to start the game
-def start_game():
-    #Ensures that we can touch the global variables for our function calls
-    global player_value_global
-    global dealer_value_global
+def start_current_game():
+    #Sets the dealer/player value to 0
+    dealer_value_global = 0
+    player_value_global = 0
+
+    #Clears the list to ensure we get brand new cards
+    player_cards.clear()
+    dealer_cards.clear()
 
     #Add 2 cards to dealer and player in the correct order of the blackjack rules
     dealer_cards.append(deal())
     player_cards.append(deal())
     dealer_cards.append(deal())
     player_cards.append(deal())
-
     
     #Iterate through dealer_cards and find the value so that we can check the sum of the cards
     for i in dealer_cards:
         dealer_value_global += card_value(i)
-    print(dealer_cards[0] + "    [HIDDEN CARD]")
+    print(dealer_cards[0] + "  [HIDDEN CARD]")
 
     #Iterate through player_cards and find the value so that we can check the sum of the cards   
     for i in player_cards:
         player_value_global += card_value(i)
 
     if dealer_value_global == 21 and player_value_global == 21:
+        print(dealer_cards[0] + "   " + dealer_cards [1])
+        print(player_cards[0] + "   " + player_cards [1])
         print("It's a tie, you get your money back!")
         return "Tie"
 
@@ -138,23 +139,22 @@ def start_game():
         print("Player got a blackjack!")
         return "Player blackjack"
 
-    elif dealer_value_global == 21:
-        print("Dealer got a blackjack!")
-        return "Dealer"
-
-    print(player_cards[0] + "    " + player_cards[1])
+    print(player_cards[0] + "   " + player_cards[1])
     print("You currently have: ", player_value_global)
 
     #Call the player_deal() function, now the player can play
-    player_value_global = player_deal(player_value_local = player_value_global)
+    player_value_global = player_deal(player_value_global)
     print("player value (global) 1: ", player_value_global)
-
+    print(dealer_cards[0] + "   " + dealer_cards [1])
+    if dealer_value_global == 21:
+        print("Dealer got a blackjack!")
+        return "Dealer"
     if player_value_global > 21:
         return "Dealer"
 
     #When the player is done playing, call dealer_deal() function, now the dealer plays
-    dealer_value_global = dealer_deal(dealer_value_local = dealer_value_global)
-
+    dealer_value_global = dealer_deal(dealer_value_global)
+    
     if dealer_value_global > player_value_global and dealer_value_global < 22:
         return "Dealer"
     elif player_value_global > dealer_value_global and player_value_global < 22:
@@ -169,10 +169,7 @@ def start_game():
 
 #Function that determines the rules of the game
 def game_rules():
-    global chips
-
     game_round = 0
-    
 
     if game_round == 0:
         chips = int(input("How much would like to start with: "))
@@ -182,14 +179,13 @@ def game_rules():
 
     while game_round > 0:
         print("Your total amount of chips: ", chips)
-        
 
         chips_betted = 0
         chips_betted = int(input("How much would you like to bet:\n"))
-        print("You have betted: ", chips_betted, "\n Your total amount of chips is: ", chips)
+        print("You have betted: ", chips_betted)
         
         create_deck()
-        result = start_game()
+        result = start_current_game()
         
         if result == "Player blackjack":
             print("Blackjack!")
@@ -218,8 +214,7 @@ def game_rules():
             print("Thank you for playing! \nToday you have walked out with: ", chips - initial_chips, " chips!")
             exit()  
 
-#Call the game_rules() function to start the game
-game_rules()
 
 if __name__ == "__main__":
-    pass
+    #Call the game_rules() function to start the game
+    game_rules()
