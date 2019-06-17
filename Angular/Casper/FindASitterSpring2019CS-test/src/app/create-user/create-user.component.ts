b1f47da373490800e05d240339dd4827910cbbee
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2019-06-17 13:57:58 +0200

    birthdate need update, temp is working (pattern)

--- Angular/Casper/FindASitterSpring2019CS-test/src/app/create-user/create-user.component.ts
+++ Angular/Casper/FindASitterSpring2019CS-test/src/app/create-user/create-user.component.ts
@@ -26,8 +26,9 @@ export class CreateUserComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
       email: ['', [Validators.required, Validators.email]],
+      // TODO: dropdown på gender
       gender: [Gender.MALE, Validators.required],
-      birthDate: ['', Validators.required],
+      birthDate: ['', [Validators.required, Validators.pattern('^(?:(?:10|12|0?[13578])/(?:3[01]|[12][0-9]|0?[1-9])/(?:1[8-9]\\d{2}|[2-9]\\d{3})|(?:11|0?[469])/(?:30|[12][0-9]|0?[1-9])/(?:1[8-9]\\d{2}|[2-9]\\d{3})|0?2/(?:2[0-8]|1[0-9]|0?[1-9])/(?:1[8-9]\\d{2}|[2-9]\\d{3})|0?2/29/[2468][048]00|0?2/29/[3579][26]00|0?2/29/[1][89][0][48]|0?2/29/[2-9][0-9][0][48]|0?2/29/1[89][2468][048]|0?2/29/[2-9][0-9][2468][048]|0?2/29/1[89][13579][26]|0?2/29/[2-9][0-9][13579][26])$') ]],
       phoneNumbers: this.fb.array([]),
       country: ['']
     })
